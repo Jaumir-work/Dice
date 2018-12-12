616905ff571549e456ed95dc367df000ed8c3bb1
Jaumir <jaumir.dasilveirajunior@emc.com>
2018-12-12 10:39:20 -0200

    Initial commit.

---
+++ GetValues/getValues.cs
@@ -0,0 +1,41 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Dice
+{
+    public static class Dice
+    {
+        [FunctionName("GetValues")]
+        public static IActionResult GetValues(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Queue("latestdraw", Connection = "AzureWebJobsStorage")] CloudQueue queue,
+            ILogger log)
+        {
+            string lastValue = null;
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            try
+            {
+                var lastMessage = queue.GetMessageAsync().Result;
+                lastValue = lastMessage.InsertionTime + " - " + lastMessage.AsString;
+
+                queue.DeleteMessageAsync(lastMessage).Wait();
+
+                lastMessage = queue.GetMessageAsync().Result;
+                lastValue = lastValue + " - " + lastMessage.InsertionTime + " - " + lastMessage.AsString;
+
+                queue.DeleteMessageAsync(lastMessage).Wait();
+            }
+            catch { }
+
+            return lastValue != null
+                ? (ActionResult)new OkObjectResult(lastValue)
+                : new BadRequestObjectResult("Could not get values from the Queue");
+        }
+    }
+}
---
+++ ThrowDice/ThrowDice.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.Extensions.Logging;
+
+namespace Dice
+{
+    public static class Dice
+    {
+        [FunctionName("ThrowDice")]
+        public static void ThrowDice([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer,
+             [Queue("latestdraw", Connection = "AzureWebJobsStorage")] CloudQueue myQueue,
+             ILogger log)
+        {
+            Random rnd = new Random();
+
+            var newDraw = rnd.Next(1, 7).ToString();
+            myQueue.AddMessageAsync(new CloudQueueMessage(newDraw));
+
+            newDraw = rnd.Next(1, 7).ToString();
+            myQueue.AddMessageAsync(new CloudQueueMessage(newDraw));
+
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
+
