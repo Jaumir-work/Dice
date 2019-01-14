61cf78cb4b23bb9bbff91fee40648d6f4449f28a
Jaumir Valença da Silveira Junior <jaumir@outlook.com>
2019-01-13 22:23:39 -0200

    Changing dashes

--- GetValues/getValues.cs
+++ GetValues/getValues.cs
@@ -22,12 +22,12 @@ namespace Dice
             try
             {
                 var lastMessage = queue.GetMessageAsync().Result;
-                lastValue = lastMessage.InsertionTime + " - " + lastMessage.AsString;
+                lastValue = lastMessage.InsertionTime + " -- " + lastMessage.AsString;
 
                 queue.DeleteMessageAsync(lastMessage).Wait();
 
                 lastMessage = queue.GetMessageAsync().Result;
-                lastValue = lastValue + " - " + lastMessage.InsertionTime + " -- " + lastMessage.AsString;
+                lastValue = lastValue + " - " + lastMessage.InsertionTime + " --- " + lastMessage.AsString;
 
                 queue.DeleteMessageAsync(lastMessage).Wait();
             }
