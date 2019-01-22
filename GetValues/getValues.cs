9d82279f29689b5906142f17ca96ce6ceb859fb0
Jaumir Valença da Silveira Junior <jaumir@outlook.com>
2019-01-22 18:32:16 -0200

    minuses

--- GetValues/getValues.cs
+++ GetValues/getValues.cs
@@ -22,12 +22,12 @@ namespace Dice
             try
             {
                 var lastMessage = queue.GetMessageAsync().Result;
-                lastValue = lastMessage.InsertionTime + " 0000----0000 " + lastMessage.AsString;
+                lastValue = lastMessage.InsertionTime + " ---- " + lastMessage.AsString;
 
                 queue.DeleteMessageAsync(lastMessage).Wait();
 
                 lastMessage = queue.GetMessageAsync().Result;
-                lastValue = lastValue + " - " + lastMessage.InsertionTime + " 0000----0000 " + lastMessage.AsString;
+                lastValue = lastValue + " - " + lastMessage.InsertionTime + " ---- " + lastMessage.AsString;
 
                 queue.DeleteMessageAsync(lastMessage).Wait();
             }
