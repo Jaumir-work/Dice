9da1f8b37ba078fcd065b5be70e01951e2d2de8f
Jaumir <jaumir.dasilveirajunior@emc.com>
2019-02-01 12:55:46 -0200

    ccc 4

--- GetValues/getValues.cs
+++ GetValues/getValues.cs
@@ -22,12 +22,12 @@ namespace Dice
             try
             {
                 var lastMessage = queue.GetMessageAsync().Result;
-                lastValue = lastMessage.InsertionTime + " bbb " + lastMessage.AsString;
+                lastValue = lastMessage.InsertionTime + " ccc " + lastMessage.AsString;
 
                 queue.DeleteMessageAsync(lastMessage).Wait();
 
                 lastMessage = queue.GetMessageAsync().Result;
-                lastValue = lastValue + " - " + lastMessage.InsertionTime + " bbb " + lastMessage.AsString;
+                lastValue = lastValue + " - " + lastMessage.InsertionTime + " ccc " + lastMessage.AsString;
 
                 queue.DeleteMessageAsync(lastMessage).Wait();
             }
